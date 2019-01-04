61abb4c2d91968e9f5b90589000173e71157b1fb
sdim-ua <ingersol@yandex.ua>
2019-01-04 11:09:37 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,11 @@
+/* 
+Я так давно родился
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println("MAY 1 2012");
+    }
+}
+
